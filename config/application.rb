82e78d149a7cb8d13efe8065e2a0890418cc8a4b
Jan Pawellek <2392542+janpawellek@users.noreply.github.com>
2023-02-09 00:43:35 +0100

    Make time zone configurable and set UTC as default

--- config/application.rb
+++ config/application.rb
@@ -8,16 +8,14 @@ end
 
 module Weltverbessern
   class Application < Rails::Application
-
-    # --- Standard Rails Config ---
-    config.time_zone = 'Pacific Time (US & Canada)'
-    config.encoding = "utf-8"
+    config.time_zone = Settings.time_zone || 'UTC'
+    config.encoding = 'utf-8'
     config.filter_parameters += [:password]
     # Enable the asset pipeline
     config.assets.enabled = true
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    # --- Standard Rails Config ---
+
     ActiveRecord.legacy_connection_handling = false
 
     config.i18n.available_locales = Settings.available_locales || [:en, :de]
