e8af1cf03564f81da326b5d6b28b001c1d9b05fd
Jan Pawellek <2392542+janpawellek@users.noreply.github.com>
2023-02-18 19:34:34 +0100

    Add pre-defined image variants to deliver different image sizes

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,6 +1,11 @@
 class ImageController < ApplicationController
   include Rails.application.routes.url_helpers
 
+  ImageVariants = {
+    'video_placeholder' => [512, 385],
+    'key_points' => [700, 500]
+  }
+
   def redirect
     image = Image.where(
       'key = ? and (locale = ? or locale = ? or locale is null)',
@@ -10,10 +15,9 @@ class ImageController < ApplicationController
     ).first
     raise ActionController::RoutingError.new('Not Found') unless image.present?
 
-    width = params[:width]
-    height = params[:height]
-    if width or height then
-      redirect_to url_for image.file.variant(resize_to_limit: [width, height])
+    variant = params[:for]
+    if variant and ImageVariants.has_key?(variant) then
+      redirect_to url_for image.file.variant(resize_to_limit: ImageVariants[variant])
     else
       redirect_to url_for image.file
     end
