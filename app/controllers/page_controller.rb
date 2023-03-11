c6b0edc0c6242e40697db1c334834039ee66c2aa
Jan Pawellek <2392542+janpawellek@users.noreply.github.com>
2023-03-11 22:04:54 +0100

    Allow frontend routing on custom pages

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,10 @@
 class PageController < ApplicationController
   def page
+    # Take the full path as lookup key (e.g. aaa/bbb/ccc)
     @key = "page.#{params[:page]}_html"
+    # If it does not exist, only take the first path part (e.g. aaa) to allow for frontend routing
+    @key = "page.#{params[:page].split('/').first}_html" unless I18n.exists? @key
+    # If it still does not exist, throw 404
     raise ActionController::RoutingError.new('Not Found') unless I18n.exists? @key
   end
 end
